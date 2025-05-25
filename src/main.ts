1c365408ada41d30d454d4be825149cfb0e1c72b
SnowDream39 <759723417@qq.com>
2025-05-25 17:26:13 +0800

    调整样式，部分改用tailwind

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import '@/styles/global.scss'
+import '@/styles/tailwind.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,27 +2,34 @@
 </script>
 
 <template>
-  <div class="wrapper">
-    <div class="header">
-      <div class="title">Earth Daily</div>
+  <div class="wrapper h-full w-full m-0 p-0 overflow-y-auto flex flex-col items-center">
+    <header class="h-[65px] w-[90%] flex justify-between items-center">
+      <div class="title text-4 font-bold uppercase">Earth Daily</div>
       <div class="right">
-        <div class="menu">Why Earth Daily?</div>
-        <div class="menu">Learn More</div>
+        <div class="menu text-4 uppercase flex">Why Earth Daily?</div>
+        <div class="menu text-4 uppercase flex">Learn More</div>
         <div class="btn_primary">Login</div>
       </div>
-    </div>
-    <div class="banner">
-      <div class="title-wrapper">
-        <div class="title">EARTH DAILY</div>
-        <div class="subtitle">UNFOLDED NEWs</div>
+    </header>
+    <main class="w-[90%] flex flex-col">
+      <div class="banner">
+        <div class="title-wrapper border-w-4">
+          <div class="title text-7xl lg:text-9xl">EARTH DAILY</div>
+          <div class="subtitle text-5xl lg:text-7xl">UNFOLDED NEWs</div>
+        </div>
       </div>
-      <div class="lines"></div>
-      <div class="btn_success" id="btn-get-started">Get Started</div>
-      <div class="right-box">
-        <div class="globe"></div>
+      <div class="flex justify-between py-4 border-t-2 gap-x-4">
+
+        <div class="left-box flex-1 h-80 border-t-2">
+          <div class="h-60">说明文字……………………………………………………</div>
+          <div class="btn_success w-30" id="btn-get-started">Get Started</div>
+        </div>
+        <div class="right-box border-2 rounded-xl ">
+          <div class="globe"></div>
+        </div>
       </div>
-    </div>
-    <footer class="footer">
+    </main>
+    <footer>
       <div class="footer-content">
         <div class="footer-item">
           <© 2025 Your Company </div>
@@ -42,46 +49,31 @@
 
 <style lang="scss" scoped>
 .wrapper {
-  //background-color: #ffffff9b;
   background-color: $color-base;
-  //background-color: #f3e7cc;
   background-image: url("https://www.transparenttextures.com/patterns/paper-2.png");
-  //background-image: url("https://www.transparenttextures.com/patterns/paper-2.png");
-  min-height: 150vh;
-  min-width: max-content;
-  display: flex;
-  flex-direction: column;
-  overflow-y: auto;
-  margin: 0;
-  padding: 0;
   background-position: center;
 }
 
-.header {
-  height: 65px;
-  width: 90%;
-  box-sizing: border-box;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  position: relative;
+a {
+  color: #80b3ff; // 链接颜色，蓝色系
+  text-decoration: none;
+  margin: 0 8px;
+
+  &:hover {
+    text-decoration: underline;
+    color: #aaddff;
+  }
+}
+
+header {
   border-bottom: 3px solid $color-font-base;
-  top: 0;
-  left: 5%;
-  right: 5%;
 
   .title {
-    font-size: 18px;
-    font-weight: bold;
     color: $color-font-base;
-    text-transform: uppercase;
   }
 
   .menu {
-    font-size: 18px;
     color: $color-font-base;
-    text-transform: uppercase;
-    display: flex;
 
     &:hover {
       color: $color-primary;
@@ -89,7 +81,63 @@
   }
 }
 
-.footer {
+.banner {
+  width: 90%;
+  margin: 0 auto; // 整体居中容器
+  position: relative;
+  min-height: 150px;
+  //font-family: $subtitle-font;
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+
+  .title-wrapper {
+    width: fit-content; // 宽度包裹内容
+    margin: 0 auto; // 让容器居中
+    vertical-align: bottom; // 有时候帮字体往下靠
+    display: inline-block;
+  }
+
+  .title {
+    font-family: $font-family-header; //font-Cheading;
+    //font-weight: bold;
+    letter-spacing: 3px;
+    margin: 0;
+    text-align: left;
+    padding-bottom: 5px;
+  }
+
+  .subtitle {
+    font-family: font-secondary;
+    font-size: 48px;
+    font-weight: 300;
+    letter-spacing: 1px;
+    margin: 0;
+    padding-top: 0;
+    text-transform: capitalize;
+    text-align: left;
+  }
+
+
+  .right-box {
+    border: 2.5px solid $color-border;
+    border-radius: 20px;
+    perspective: 800px;
+  }
+
+  .lines {
+    border-top: 2px solid $color-border;
+    position: relative;
+
+    &::after {
+      content: "";
+      background-color: $color-border;
+    }
+  }
+}
+
+
+footer {
   width: 100%;
   background-color: #222; // 深色背景
   color: #eee; // 浅色文字
@@ -98,16 +146,7 @@
   font-size: 1rem;
   border-top: 2px solid #444; // 顶部分割线
 
-  a {
-    color: #80b3ff; // 链接颜色，蓝色系
-    text-decoration: none;
-    margin: 0 8px;
 
-    &:hover {
-      text-decoration: underline;
-      color: #aaddff;
-    }
-  }
 
   .footer-content {
     max-width: 1200px; // 最大宽度，居中显示
@@ -158,78 +197,6 @@
   width: 40%;
 }
 
-.banner {
-  width: 90%;
-  margin: 0 auto; // 整体居中容器
-  position: relative;
-  min-height: 150px;
-  //font-family: $subtitle-font;
-  display: flex;
-  flex-direction: column;
-  flex: 1;
-
-  .title-wrapper {
-    width: fit-content; // 宽度包裹内容
-    margin: 0 auto; // 让容器居中
-    vertical-align: bottom; // 有时候帮字体往下靠
-    display: inline-block;
-  }
-
-  .title {
-    font-family: $font-family-header; //font-Cheading;
-    //font-weight: bold;
-    font-size: 128px;
-    letter-spacing: 3px;
-    margin: 0;
-    height: 155px;
-    text-align: left;
-    padding-bottom: 5px;
-  }
-
-  .subtitle {
-    font-family: font-secondary;
-    font-size: 48px;
-    font-weight: 300;
-    letter-spacing: 1px;
-    margin: 0;
-    padding-top: 0;
-    text-transform: capitalize;
-    text-align: left;
-  }
-
-  #btn-get-started {
-    position: absolute;
-    top: 590px;
-    left: 60px;
-  }
-
-  .right-box {
-    position: absolute;
-    top: 260px;
-    right: 0px;
-    width: 380px;
-    height: 400px;
-    border: 2.5px solid $color-border;
-    border-radius: 20px;
-    perspective: 800px;
-  }
-
-  .lines {
-    border-top: 2px solid $color-border;
-    position: relative;
-
-    &::after {
-      content: "";
-      position: absolute;
-      top: 30px;
-      left: 0;
-      height: 2px;
-      width: 70%;
-      background-color: $color-border;
-    }
-  }
-}
-
 .globe-container {
   perspective: 800px; // 观察距离增加3D感觉
   width: 200px;
@@ -237,8 +204,8 @@
 }
 
 .globe {
-  width: 100%;
-  height: 100%;
+  width: 300px;
+  height: 300px;
   border-radius: 50%;
   background-image: url('/NASA2.jpg');
   background-size: cover;
