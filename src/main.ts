a5a9af8ac3a2e0890dba6da5355f955e5cd07efc
SnowDream39 <759723417@qq.com>
2025-06-06 12:26:58 +0800

    移除不用的依赖

--- src/main.ts
+++ src/main.ts
@@ -17,10 +17,4 @@ import 'live2d-widget/style.css'
 app.use(live2d)
 
 
-import * as ElementPlusIconsVue from '@element-plus/icons-vue'
-for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
-  app.component(key, component)
-}
-
-
 app.mount('#app')
